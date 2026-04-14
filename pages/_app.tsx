caa0e938fb7b461bd4ef14432b9fa64cee5e194d
Alec Joseph <linkandmarth789@gmail.com>
2026-04-14 16:18:35 -0700

    Adds home button to all pages but home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import type { AppProps } from 'next/app';
 import '../app/globals.css';
+import Link from 'next/link';
+import Button from '../app/components/Button'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <div className="fixed top-4 right-4 w-40">
+        <Link href='/'><Button text={"Home"} /></Link>
+      </div>
+      <div className="pt-16">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
