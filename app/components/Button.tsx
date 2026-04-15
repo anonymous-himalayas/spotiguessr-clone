f7490dc865d7f3042a97ddec979e7388320f24a6
Alec Joseph <linkandmarth789@gmail.com>
2026-04-14 17:19:14 -0700

    Adds CodeInput component, allows disabling buttons, styles enter-code page

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,8 +1,15 @@
-export default function Button({ text }: {text: string}) {
+type ButtonProps = {
+  text:string;
+  disabled?: boolean;
+}
+
+export default function Button({ text, disabled }: ButtonProps) {
   return (
-    <div className="h-full bg-gray-900 p-5 rounded-xl border border-gray-800
-                    hover:border-green-500 hover:-translate-y-1
-                    transition duration-200 shadow-lg">
+    <div className={`bg-gray-900 px-5 py-2 rounded-xl border border-gray-800 transition duration-200 shadow-lg
+                    ${disabled
+                      ? 'opacity-50 cursor-not-allowed'
+                      : 'hover:border-green-500 hover:-translate-y-1 cursor-pointer'
+                    }`}>
       <h3 className="text-lg font-semibold mb-1 flex justify-center items-center">{text}</h3>
     </div>
   )
--- pages/enter-code.tsx
+++ pages/enter-code.tsx
@@ -1,7 +1,23 @@
 import Link from "next/link";
+import CodeInput from "../app/components/CodeInput";
+import Button from "../app/components/Button";
+import { useState } from "react";
 
 export default function EnterCode() {
+  const [code, setCode] = useState("");
   return (
-    <Link href="/lobby">Lobby</Link>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Enter Room Code</h1>
+      <p className="pt-4 pb-4 text-gray-500">Ask the host for the 6-digit code to join their party</p>
+      <div>
+        <CodeInput value={code} onChange={setCode} />
+        <div className="h-14">
+          {code.length === 11
+            ? <Link href='/lobby'><Button text="Join Party" /></Link>
+            : <Button text="Join Party" disabled />
+          }
+        </div>
+      </div>
+    </div>
   )
 }
